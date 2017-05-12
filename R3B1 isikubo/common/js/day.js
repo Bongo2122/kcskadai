2b10ebfbb4d16f15affad8b11d530301a66397f4
Bongo2122 <isikubo1122@gmail.com>
2017-05-12 22:23:26 +0900

    Add files via upload

---
+++ R3B1 isikubo/common/js/day.js
@@ -0,0 +1,10 @@
+﻿
+var hiduke=new Date(); 
+
+var year = hiduke.getFullYear();
+var month = hiduke.getMonth()+1;
+var week = hiduke.getDay();
+var day = hiduke.getDate();
+
+var yobi= new Array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+document.write(year+"年"+month+"月"+day+"日 ("+yobi[week]+") ");
